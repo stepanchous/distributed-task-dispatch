c4f9c0264e17cf24b01eb8cb7bec62eabedde11f
stepanchous <s.kolovorotnyy@gmail.com>
2024-02-07 20:48:50 +0300

    made json file with variables

--- database/setup_database.py
+++ database/setup_database.py
@@ -1,5 +1,6 @@
 import sqlite3
 import db_pb2
+import json
 
 with sqlite3.connect("database.db") as conn:
     cursor = conn.cursor()
@@ -20,25 +21,23 @@ with sqlite3.connect("database.db") as conn:
         );
     ''')
 
-    l_list = db_pb2.List()
-    l_list.values.extend([1, 2, 3, 4, 5])
-    l_serialized = db_pb2.Field()
-    l_serialized.list.CopyFrom(l_list)
-
-    k_list = db_pb2.List()
-    k_list.values.extend([-6, -7, -11, -9, -10])
-    k_serialized = db_pb2.Field()
-    k_serialized.list.CopyFrom(k_list)
-
-    x_scalar = db_pb2.Scalar()
-    x_scalar.value = 420
-    x_serialized = db_pb2.Field()
-    x_serialized.scalar.CopyFrom(x_scalar)
-
     query = "INSERT INTO static_data (identifier, data) VALUES (?, ?)"
 
-    cursor.execute(query, ("L", l_serialized.SerializeToString()))
-    cursor.execute(query, ("K", k_serialized.SerializeToString()))
-    cursor.execute(query, ("x", x_serialized.SerializeToString()))
+    with open("data.json") as f:
+        var_to_value = json.load(f)
+
+        for var, value in var_to_value.items():
+            field = db_pb2.Field()
+            if var.islower():
+                value_pb = db_pb2.Scalar()
+                value_pb.value = value
+                field.scalar.CopyFrom(value_pb)
+            else:
+                list_pb = db_pb2.List()
+                for el in value:
+                    list_pb.values.append(el)
+                field.list.CopyFrom(list_pb)
+
+            cursor.execute(query, (var, field.SerializeToString()))
 
     conn.commit()
